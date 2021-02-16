89b16fb5bc99fed9885cfc673c318c5997fa4787
horihiro <hori.kawa.shima.tool+github@gmail.com>
2021-02-16 10:33:16 +0900

    check update datetime

--- showEnglishBalloon.user.js
+++ showEnglishBalloon.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://hihorika.net/
 // @version      0.1
 // @description  Show engish sentense on mouse over in Azure Docs
-// @author       hihorika@microsoft.com a.k.a. horihiro 
+// @author       hihorika@microsoft.com a.k.a. horihiro
 // @match        https://docs.microsoft.com/ja-jp/azure/*
 // @downloadURL  https://github.com/horihiro/showEnglishBalloonAzureDocs/raw/main/showEnglishBalloon.user.js
 // @updateURL    https://github.com/horihiro/showEnglishBalloonAzureDocs/raw/main/showEnglishBalloon.meta.js
@@ -11,38 +11,53 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    if (!document.querySelectorAll('.lang-toggle-container')) return;
+(async function() {
+  'use strict';
+  if (!document.querySelectorAll('.lang-toggle-container')) return;
 
-    const link4balloon = document.createElement('link');
-    link4balloon.setAttribute('rel', 'stylesheet');
-    link4balloon.setAttribute('href', 'https://unpkg.com/balloon-css/balloon.min.css');
+  const updateDateElm = document.querySelectorAll('time[data-article-date-source="ms.date"]')[0];
+  if (updateDateElm) {
+    const updateDate = new Date(updateDateElm.getAttribute('datetime'));
+    if (updateDateElm && updateDate.getFullYear() > 2000) {
+      const urlEnUs = document.location.href.replace(/^(https:\/\/docs.microsoft.com\/)[^/]+(\/.*)$/, '$1en-us$2');
+      const response = await fetch(urlEnUs);
+      const body = await response.text();
+      const datetime = body.replace(/^[\w\W]*<time[^>]* datetime="([^"]+)"[^>]*>[^<]+<\/time>[\w\W]*$/, "$1");
+      const updateDateEnUs = new Date(datetime);
+      if (updateDate.getTime() < updateDateEnUs.getTime()) {
+        updateDateElm.innerHTML = `<a href="${urlEnUs}" style="--balloon-color: red;" aria-label="英語版は ${updateDateEnUs.getFullYear()}/${updateDateEnUs.getMonth()+1}/${updateDateEnUs.getDate()} に更新されています" data-balloon-pos="up" target="_blank" rel="noopener noreferrer">⚠️${updateDateElm.innerText}</a>`;
+      }
+    }
+  }
 
-    const style4balloon = document.createElement('style');
-    style4balloon.innerText = `
+  const link4balloon = document.createElement('link');
+  link4balloon.setAttribute('rel', 'stylesheet');
+  link4balloon.setAttribute('href', 'https://unpkg.com/balloon-css/balloon.min.css');
+
+  const style4balloon = document.createElement('style');
+  style4balloon.innerText = `
 :root {
-  --balloon-border-radius: 0;
-  --balloon-color: blue;
-  --balloon-font-size: 15px;
+--balloon-border-radius: 0;
+--balloon-color: blue;
+--balloon-font-size: 15px;
 }}
 
 span[data-ttu-id] {
-  text-decoration: none;
+text-decoration: none;
 }
 span[data-ttu-id]:hover {
-  text-decoration: underline;
-  cursor: default;
+text-decoration: underline;
+cursor: default;
 }
 `;
 
-    document.body.appendChild(link4balloon);
-    document.body.appendChild(style4balloon);
-    Array.prototype.forEach.call(document.querySelectorAll('span[data-ttu-id]'), (elm) => {
-        elm.setAttribute('aria-label', elm.nextSibling.innerText);
-        elm.setAttribute('data-balloon-pos', 'up');
-        // elm.title = elm.nextSibling.innerText
-    });
+  document.body.appendChild(link4balloon);
+  document.body.appendChild(style4balloon);
+  Array.prototype.forEach.call(document.querySelectorAll('span[data-ttu-id]'), (elm) => {
+      elm.setAttribute('aria-label', elm.nextSibling.innerText);
+      elm.setAttribute('data-balloon-pos', 'up');
+      // elm.title = elm.nextSibling.innerText
+  });
 
-    return;
+  return;
 })();
